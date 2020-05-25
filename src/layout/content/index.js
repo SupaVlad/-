42447e6defed5fe943f7dccc1bc341ca7e063f43
Владислав Бородин <vb@rocketstation.ru>
2020-05-25 16:00:22 +0300

    promo

--- src/layout/content/index.js
+++ src/layout/content/index.js
@@ -1,1 +1,67 @@
 import './content.sass';
+import initHome from '../../pages/home';
+
+export default () => {
+  $(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+      dots: true,
+      loop:true,
+      items:3,
+      margin:135,
+      responsiveClass:true,
+      responsive:{
+        0:{
+          items:1,
+          nav:true,
+          margin:10,
+        },
+        600:{
+          items:2,
+          nav:false,
+          margin:120,
+        },
+        1000:{
+          items:2,
+          nav:true,
+          loop:true,
+          center: true,
+          autoWidth: true,
+          margin:20,
+        }
+      }
+    });
+    $('.popup-gallery').magnificPopup({
+      delegate: 'a',
+      type: 'image',
+      tLoading: 'Loading image #%curr%...',
+      mainClass: 'mfp-img-mobile',
+      gallery: {
+        enabled: true,
+        navigateByImgClick: true,
+        preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+      }
+    });
+    $('.popup-gallery-thecond').magnificPopup({
+      delegate: 'a',
+      type: 'image',
+      tLoading: 'Loading image #%curr%...',
+      mainClass: 'mfp-img-mobile',
+      gallery: {
+        enabled: true,
+        navigateByImgClick: true,
+        preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+      }
+    });
+    $('.popup-gallery-third').magnificPopup({
+      delegate: 'a',
+      type: 'image',
+      tLoading: 'Loading image #%curr%...',
+      mainClass: 'mfp-img-mobile',
+      gallery: {
+        enabled: true,
+        navigateByImgClick: true,
+        preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+      }
+    });
+  });
+};
--- src/layout/index.js
+++ src/layout/index.js
@@ -4,7 +4,10 @@ import './wrapper';
 import './content';
 import './header';
 import './footer';
+import initCarousel from './content';
+import 'owl.carousel/dist/assets/owl.carousel.css';
 
 export default () => {
+  initCarousel();
   console.log('init layout');
 };
